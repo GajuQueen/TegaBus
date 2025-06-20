5f1263fc66091e4190bbff9d577ad21d559cb029
GajuQueen <gajuq7@gmail.com>
2025-06-20 15:58:45 +0200

    changes

--- src/main/java/org/example/tegabus/Bus/BusService.java
+++ src/main/java/org/example/tegabus/Bus/BusService.java
@@ -3,10 +3,15 @@ package org.example.tegabus.Bus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class BusService {
     public final BusRepository busRepository;
 
+    public Bus findBusById(UUID id) {
+        return busRepository.findById(id).orElseThrow(() -> new RuntimeException("Bus not found"));
+    }
 
 }
