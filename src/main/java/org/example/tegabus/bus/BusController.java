c8699452fadb7ac7ce415256e918fd82bab8b23b
GajuQueen <gajuq7@gmail.com>
2025-07-02 10:03:40 +0200

    new changes on bus

--- src/main/java/org/example/tegabus/bus/BusController.java
+++ src/main/java/org/example/tegabus/bus/BusController.java
@@ -18,36 +18,70 @@ public class BusController {
 
     @PostMapping
     @Operation(summary = "Create a Bus")
-    public ResponseEntity<Bus> createBus(@RequestBody @Valid BusDto dto){
-     Bus bus = busService.createBus(dto);
-        return new ResponseEntity<>(bus, HttpStatus.CREATED);
+    public ResponseEntity<BusResponseDto> createBus(@RequestBody @Valid BusDto dto) {
+        Bus bus = busService.createBus(dto);
+        BusResponseDto responseDto = BusResponseDto.builder()
+                .id(bus.getId())
+                .model(bus.getModel())
+                .status(bus.getStatus())
+                .type(bus.getType())
+                .driverName(bus.getDriverName())
+                .driverPhoneNumber(bus.getDriverPhoneNumber())
+                .build();
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
 
     @GetMapping
     @Operation(summary = "Get all Buses")
-    public ResponseEntity<List<Bus>> findAll() {
+    public ResponseEntity<List<BusResponseDto>> findAll() {
         List<Bus> buses = busService.findAll();
-        return new ResponseEntity<>(buses, HttpStatus.OK);
+        List<BusResponseDto> responseDto = buses.stream()
+                .map(bus -> BusResponseDto.builder()
+                        .id(bus.getId())
+                        .model(bus.getModel())
+                        .status(bus.getStatus())
+                        .type(bus.getType())
+                        .driverName(bus.getDriverName())
+                        .driverPhoneNumber(bus.getDriverPhoneNumber())
+                        .build())
+                .toList();
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     @Operation(summary = "Find Bus by id")
-    public ResponseEntity<Bus> findBusById(@PathVariable UUID id){
+    public ResponseEntity<BusResponseDto> findBusById(@PathVariable UUID id) {
         Bus bus = busService.findBusById(id);
-        return new ResponseEntity<>(bus, HttpStatus.OK);
+        BusResponseDto responseDto = BusResponseDto.builder()
+                .id(bus.getId())
+                .model(bus.getModel())
+                .status(bus.getStatus())
+                .type(bus.getType())
+                .driverName(bus.getDriverName())
+                .driverPhoneNumber(bus.getDriverPhoneNumber())
+                .build();
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     @PatchMapping("/{id}")
     @Operation(summary = "Update a Bus")
-    public ResponseEntity<Bus> updateBus(@PathVariable UUID id, @RequestBody @Valid BusDto dto){
+    public ResponseEntity<BusResponseDto> updateBus(@PathVariable UUID id, @RequestBody @Valid BusDto dto) {
         Bus bus = busService.updateBus(id, dto);
-        return new ResponseEntity<>(bus, HttpStatus.OK);
+        BusResponseDto responseDto = BusResponseDto.builder()
+                .id(bus.getId())
+                .model(bus.getModel())
+                .status(bus.getStatus())
+                .type(bus.getType())
+                .driverName(bus.getDriverName())
+                .driverPhoneNumber(bus.getDriverPhoneNumber())
+                .build();
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete Bus by id")
-    public ResponseEntity<Void> deleteById(@PathVariable UUID id){
-    busService.deleteById(id);
-    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    public ResponseEntity<Void> deleteById(@PathVariable UUID id) {
+        busService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- src/main/java/org/example/tegabus/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/tegabus/exception/GlobalExceptionHandler.java
@@ -1,4 +1,83 @@
 package org.example.tegabus.exception;
 
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-}
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException e, HttpServletRequest req) {
+
+        var response = new ErrorResponse();
+        response.setMessage(e.getMessage());
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        response.setPath(req.getRequestURI());
+        response.setTimestamp(LocalDateTime.now());
+
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest req) {
+        List<String> validationErrors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
+
+        var response = new ErrorResponse();
+        response.setMessage(e.getMessage());
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setPath(req.getRequestURI());
+        response.setTimestamp(LocalDateTime.now());
+        response.setErrors(validationErrors);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e, HttpServletRequest req) {
+        var response = new ErrorResponse();
+        response.setMessage(e.getMessage());
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setPath(req.getRequestURI());
+        response.setTimestamp(LocalDateTime.now());
+
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e, HttpServletRequest req) {
+        var response = new ErrorResponse();
+        response.setMessage(e.getMessage());
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setPath(req.getRequestURI());
+        response.setTimestamp(LocalDateTime.now());
+
+        return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e, HttpServletRequest req) {
+//        e.printStackTrace();
+        var response = new ErrorResponse();
+        response.setMessage(e.getMessage());
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.setPath(req.getRequestURI());
+        response.setTimestamp(LocalDateTime.now());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+   }
