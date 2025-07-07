084ba4b194e1eaee8795ce31b4b1c68935ef9f27
NDAYISHIMIYE Evodie <ndayishimiyeevodie@gmail.com>
2025-07-07 17:53:26 +0200

    resolve conflicts

---
+++ src/main/java/org/example/tegabus/dto/PasswordRequestDto.java
@@ -0,0 +1,8 @@
+package org.example.tegabus.dto;
+
+import lombok.Data;
+
+@Data
+public class PasswordRestRequestDto {
+    private String email;
+}
---
+++ src/main/java/org/example/tegabus/dto/PasswordResetDto.java
@@ -0,0 +1,4 @@
+package org.example.tegabus.dto;
+
+public class PasswordResetDto {
+}
