dd67f31e40f597896504049606e22fa4c0a947fb
NDAYISHIMIYE Evodie <ndayishimiyeevodie@gmail.com>
2025-07-08 14:29:52 +0200

    Resolved merrged conflict and updated Repo

--- src/main/java/org/example/tegabus/schedule/ScheduleRepository.java
+++ src/main/java/org/example/tegabus/schedule/ScheduleRepository.java
@@ -1,8 +1,7 @@
-<<<<<<<< HEAD:src/main/java/org/example/tegabus/schedule/ScheduleRepository.java
+
 package org.example.tegabus.schedule;
-========
-package org.example.tegabus.booking;
->>>>>>>> 6536998bd342cde0273072b72e348094b1d7d45f:src/main/java/org/example/tegabus/booking/BookingRepository.java
+
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
