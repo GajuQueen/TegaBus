011048fb47d4fb0ee87a3bb42d061d5ea93d281a
NDAYISHIMIYE Evodie <ndayishimiyeevodie@gmail.com>
2025-07-08 14:29:52 +0200

    Resolved merrged conflict and updated Repo

--- src/main/java/org/example/tegabus/schedule/ScheduleRepository.java
+++ src/main/java/org/example/tegabus/schedule/ScheduleRepository.java
@@ -7,5 +7,11 @@ import java.util.List;
 import java.util.UUID;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, UUID> {
+    List<Schedule> findByTravelDate(LocalDate travelDate);
+    List<Schedule> findByTravelDateAndDepartureTime(LocalDate travelDate, LocalDate departureDate);
+    List<Schedule> findByBusId(UUID busId);
+    List<Schedule> findByRouteId(UUID routeId);
+    List<Schedule> findByStatus(ScheduleStatus status);
+
     UUID id(UUID id);
 }
