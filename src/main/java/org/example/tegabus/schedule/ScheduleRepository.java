1d351361cf3caa57db0f89e9bd144c4a5ebcd170
GajuQueen <gajuq7@gmail.com>
2025-07-10 18:38:45 +0200

    Modified ScheduleRepository.java

--- src/main/java/org/example/tegabus/schedule/ScheduleRepository.java
+++ src/main/java/org/example/tegabus/schedule/ScheduleRepository.java
@@ -1,10 +1,17 @@
 
 package org.example.tegabus.schedule;
 
+import org.example.tegabus.bus.Bus;
+import org.example.tegabus.company.Company;
+import org.example.tegabus.route.Route;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, UUID> {
     UUID id(UUID id);
+    public interface BusRepository extends JpaRepository<Bus, UUID> {}
+    public interface CompanyRepository extends JpaRepository<Company, UUID> {}
+    public interface RouteRepository extends JpaRepository<Route, UUID> {}
+
 }
