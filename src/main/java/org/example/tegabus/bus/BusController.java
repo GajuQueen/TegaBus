73cc8054a3567369f28079f1a9c60c211d9137bb
GajuQueen <gajuq7@gmail.com>
2025-07-10 18:38:42 +0200

    Modified BusController.java

--- src/main/java/org/example/tegabus/bus/BusController.java
+++ src/main/java/org/example/tegabus/bus/BusController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/bus")
+@RequestMapping("/api/bus")
 @SecurityRequirement(name = "auth")
 @RequiredArgsConstructor
 public class BusController {
