73ac33900717857ecdad27a4464be6ebeb8c0c89
GajuQueen <gajuq7@gmail.com>
2025-07-10 18:38:47 +0200

    Deleted unused BookingRepository.java

--- src/main/java/org/example/tegabus/Booking/Dtos/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.tegabus.booking;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface BookingRepository extends JpaRepository<Booking, UUID> {
-}
