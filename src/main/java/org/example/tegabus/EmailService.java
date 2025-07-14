56860136f28f8e2c1a5006feda8ee7942edc4ce9
GajuQueen <gajuq7@gmail.com>
2025-07-14 18:14:35 +0200

    added verification codes in tegabus

--- src/main/java/org/example/tegabus/EmailService.java
+++ src/main/java/org/example/tegabus/EmailService.java
@@ -7,15 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
 
-//import javax.mail.internet.MimeMessage;
 @Service
 public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    // Send email with QR code as inline image or attachment
+
     public void sendEmailWithQRCode(String toEmail, String subject, String bodyText, byte[] qrCodeImage) throws Exception {
         MimeMessage message = mailSender.createMimeMessage();
 
@@ -26,5 +24,23 @@ public class EmailService {
 
         helper.addInline("qrCodeImage", new ByteArrayResource(qrCodeImage), "image/png");
         mailSender.send(message);
+    }
+
+//    VERIFICATION
+    public void sendVerificationEmail(String toEmail, String token) {
+        String subject = "Verify your Tegabus Account";
+        String body = "Click this link to verify your email:<br><br>" +
+                "<a href=\"http://localhost:8080/api/auth/verify?token=" + token + "\">Verify Email</a>";
+
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(toEmail);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            mailSender.send(message);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send verification email");
+        }
     }
 }
