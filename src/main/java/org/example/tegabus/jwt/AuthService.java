fe5c7d3f2f47a0ce61d026dc156d379c026c6c0a
GajuQueen <gajuq7@gmail.com>
2025-07-14 20:03:37 +0200

    Add OTP email verification logic in AuthService

--- src/main/java/org/example/tegabus/jwt/AuthService.java
+++ src/main/java/org/example/tegabus/jwt/AuthService.java
@@ -6,6 +6,7 @@ import org.example.tegabus.dto.RegisterRequestDto;
 import org.example.tegabus.user.Role;
 import org.example.tegabus.user.User;
 import org.example.tegabus.user.UserRepository;
+import org.example.tegabus.user.UserService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -19,6 +20,7 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
+    private final UserService userService;
 
     public User registerUser(RegisterRequestDto dto){
         if(!dto.getPassword().equals(dto.getConfirmPassword())){
@@ -26,12 +28,12 @@ public class AuthService {
         }
         User user = new User();
         user.setEmail(dto.getEmail());
-        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setPassword(dto.getPassword());
         user.setFirstName(dto.getFirstName());
         user.setLastName(dto.getLastName());
 //        user.setRole(Role.ADMIN);
         user.setRole(Role.USER);
-        return userRepository.save(user);
+        return userService.registerUser(user);
     }
     public User registerAdmin(RegisterRequestDto dto){
         if(!dto.getPassword().equals(dto.getConfirmPassword())){
@@ -39,14 +41,14 @@ public class AuthService {
         }
         User user = new User();
         user.setEmail(dto.getEmail());
-        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setPassword(dto.getPassword());
         user.setFirstName(dto.getFirstName());
         user.setLastName(dto.getLastName());
         user.setRole(Role.ADMIN);
 //        user.setRole(Role.USER);
         user.setCountry(dto.getCountry());
         user.setPhoneNumber(dto.getPhoneNumber());
-        return userRepository.save(user);
+        return userService.registerUser(user);
     }
     public User loginUser(LoginRequestDto dto){
         Authentication authentication = authenticationManager.authenticate(
