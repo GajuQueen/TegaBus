ad82cabf7630b594854706bae786a490f47cf802
NDAYISHIMIYE Evodie <ndayishimiyeevodie@gmail.com>
2025-07-18 00:13:09 +0200

    booking

--- src/main/java/org/example/tegabus/Booking/Dtos/BookingResponseDto.java
+++ src/main/java/org/example/tegabus/Booking/Dtos/BookingResponseDto.java
@@ -1,4 +1,17 @@
 package org.example.tegabus.booking.Dtos;
 
+import lombok.Builder;
+import lombok.Data;
+import org.example.tegabus.route.routeDtos.RouteResponseDto;
+
+import java.time.LocalDate;
+import java.util.UUID;
+@Data
+@Builder
 public class BookingResponseDto {
+    private UUID id;
+    private String passengerName;
+    private RouteResponseDto route;
+    private LocalDate travelDate;
+    private String seatNumber;
 }
---
+++ src/main/java/org/example/tegabus/route/RouteConverter.java
@@ -0,0 +1,4 @@
+package org.example.tegabus.route;
+
+public class RouteConverter {
+}
